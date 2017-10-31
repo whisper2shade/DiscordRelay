775587b456abe24df02a3416947764e0d8d7bf4f
whisper2shade <1337036+whisper2shade@users.noreply.github.com>
2017-10-31 21:53:40 +0100

    Internationalization, Switching JDA initalization back to blocking (couldn't get the guild fast enough and it is blocking only during initialization), Auto create kingdom and village categories and channels on discord

--- src/main/java/org/nyxcode/wurm/discordrelay/DiscordRelay.java
+++ src/main/java/org/nyxcode/wurm/discordrelay/DiscordRelay.java
@@ -1,50 +1,104 @@
 package org.nyxcode.wurm.discordrelay;
 
-import com.wurmonline.server.*;
+import com.wurmonline.server.Message;
+import com.wurmonline.server.Server;
+import com.wurmonline.server.Servers;
+import com.wurmonline.server.WurmId;
 import com.wurmonline.server.kingdom.Kingdom;
 import com.wurmonline.server.kingdom.Kingdoms;
 import com.wurmonline.server.villages.PvPAlliance;
 import com.wurmonline.server.villages.Village;
+import com.wurmonline.server.villages.Villages;
 import com.wurmonline.server.webinterface.WcKingdomChat;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.Category;
 import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.managers.GuildController;
 import org.gotti.wurmunlimited.modloader.interfaces.*;
 
 import javax.security.auth.login.LoginException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 
 /**
  * Created by whisper2shade on 22.04.2017.
  */
-public class DiscordRelay extends ListenerAdapter implements WurmServerMod, PreInitable, Configurable, ChannelMessageListener {
+public class DiscordRelay extends ListenerAdapter implements WurmServerMod, PreInitable, Configurable, ChannelMessageListener, ServerStartedListener {
 
+    ResourceBundle resourceBundle = ResourceBundle.getBundle("messages", Locale.ENGLISH);
 
     private JDA jda;
     private String botToken;
     private String serverName;
     private String wurmBotName;
     private boolean useUnderscore;
+    private Guild guild;
 
 
     public void preInit() {
         initJDA();
+        guild = jda.getGuildsByName(serverName, true).get(0);
+    }
+
+    public void onServerStarted() {
+        createKingdomChannels();
+        createVillageChannels();
+    }
+
+    private void createKingdomChannels() {
+        List<Kingdom> kingdoms = Arrays.asList(Kingdoms.getAllKingdoms());
+        Category category = createOrGetCategory(resourceBundle.getString("core.category.kingdoms"));
+        for (Kingdom kingdom : kingdoms) {
+            ensureChannel(category, kingdom.getName());
+        }
+    }
+
+    private void ensureChannel(Category category, String entityName) {
+        String channelName = discordifyName(entityName);
+        List<TextChannel> channels = guild.getTextChannelsByName(channelName, true);
+        if (channels.isEmpty()) {
+            category.createTextChannel(channelName).complete();
+        }
+    }
+
+    private Category createOrGetCategory(String name) {
+        GuildController controller = guild.getController();
+
+        List<Category> categories = guild.getCategoriesByName(name, true);
+        Category category;
+        if (categories.isEmpty()) {
+            controller.createCategory(name).complete();
+            category = guild.getCategoriesByName(name, true).get(0);
+        } else {
+            category = categories.get(0);
+        }
+        return category;
+    }
+
+    private void createVillageChannels() {
+        List<Village> villages = Arrays.asList(Villages.getVillages());
+        Category category = createOrGetCategory(resourceBundle.getString("core.category.villages"));
+        for (Village village : villages) {
+            ensureChannel(category, village.getName());
+        }
     }
 
     private void initJDA() {
         try {
-            jda = new JDABuilder(AccountType.BOT).setToken(botToken).addEventListener(this).buildAsync();
+            jda = new JDABuilder(AccountType.BOT).setToken(botToken).addEventListener(this).buildBlocking();
         } catch (LoginException e) {
             e.printStackTrace();
         } catch (RateLimitedException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
